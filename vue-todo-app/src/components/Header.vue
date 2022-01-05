df0e33ecc3c295410c98b4754350067de4efa9f7
James T. Arnold <jamestarnold85@gmail.com>
2022-01-05 10:55:47 -0600

    added vue svg

--- vue-todo-app/src/components/Header.vue
+++ vue-todo-app/src/components/Header.vue
@@ -1,6 +1,23 @@
 <template>
   <header>
-    <h1>{{ title }}</h1>
+    <div style="display: flex">
+      <svg
+        width="50px"
+        height="50px"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 261.76 226.69"
+      >
+        <path
+          d="M161.096.001l-30.225 52.351L100.647.001H-.005l130.877 226.688L261.749.001z"
+          fill="#41b883"
+        />
+        <path
+          d="M161.096.001l-30.225 52.351L100.647.001H52.346l78.526 136.01L209.398.001z"
+          fill="#34495e"
+        />
+      </svg>
+      <h1>{{ title }}</h1>
+    </div>
     <Button
       @btn-click="$emit('toggle-add-task')"
       :text="showAddTask ? 'CLOSE' : 'ADD TASK'"
